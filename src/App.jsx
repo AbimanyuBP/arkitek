526dda0701ea74a24b4f19a2b4bc4344a4abae20
AbimanyuBP <70093819+AbimanyuBP@users.noreply.github.com>
2024-06-10 16:52:19 +0700

    Another Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="text-red-600 font-extrabold">
+        <h1>AAAAAAAAAAAAAAAAAAAA</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
