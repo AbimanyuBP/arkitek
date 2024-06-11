a252b8fa06d4746d72c4a7dd27da2b246d40f30c
AbimanyuBP <70093819+AbimanyuBP@users.noreply.github.com>
2024-06-11 15:32:13 +0700

    Added new page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Home from "./pages/home/Home";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className="text-red-600 font-extrabold">
-        <h1>AAAAAAAAAAAAAAAAAAAA</h1>
-      </div>
+      <Home/>
     </>
   )
 }
