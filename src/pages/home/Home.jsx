83c34e6c2c85b73ad640c5a4eb8282f9a6af9fdc
AbimanyuBP <70093819+AbimanyuBP@users.noreply.github.com>
2024-06-14 10:03:10 +0700

    Added Mission component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
 import Navbar from '../../components/navbar/Navbar'
 import Hero from '../../components/hero/Hero'
 import About from '../../components/about/About'
+import Mission from '../../components/mission/Mission'
 
 export default function Home() {
   return (
@@ -9,6 +9,7 @@ export default function Home() {
     <Navbar/>
     <Hero/>
     <About/>
+    <Mission/>
     Home
     </>
   )
