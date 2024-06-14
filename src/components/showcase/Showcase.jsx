45accc1e1439e2949659f65a865dbd5bbc6a0c73
AbimanyuBP <70093819+AbimanyuBP@users.noreply.github.com>
2024-06-14 16:42:02 +0700

    Added Showcase component

---
+++ src/components/showcase/Showcase.jsx
@@ -0,0 +1,58 @@
+import { Box, ImageList, ImageListItem, ImageListItemBar } from "@mui/material"
+import project1 from "/assets/projects/project_1.png"
+import project2 from "/assets/projects/project_2.jpg"
+import project3 from "/assets/projects/project_3.jpg"
+import project4 from "/assets/projects/project_4.jpg"
+import project5 from "/assets/projects/project_5.jpg"
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward'
+
+
+export default function Showcase() {
+    const images = [
+        {
+            img: project1,
+            title: "Project Aulum"
+        },
+        {
+            img: project2,
+            title: "Project Navaro"
+        },
+        {
+            img: project3,
+            title: "Project Merry"
+        },
+        {
+            img: project4,
+            title: "Project Autumn"
+        },
+        {
+            img: project5,
+            title: "Project Breeze"
+        },
+    ]
+
+  return (
+    <div className="my-5 mx-20">
+        <div className="flex justify-end mb-5">
+            <h1 className="text-[50px] text-gray-500">Our Projects</h1>
+        </div>
+        <Box>
+            <ImageList variant="masonry" cols={3} gap={8}>
+                {images.map((item) => (
+                    <ImageListItem key={item.img}  className="cursor-pointer">
+                        <img src={item.img} alt={item.title}/>
+                        <ImageListItemBar
+                            title={item.title}
+                        />
+                    </ImageListItem>
+                ))}
+            </ImageList>
+        </Box>
+        <div className="flex justify-end mt-5">
+            <div className="flex justify-center items-center bg-gray-600 cursor-pointer w-[130px] h-[50px]">
+                <h2 className="ml-[20px] text-white">Read More <ArrowForwardIcon className="text-white" fontSize="small"/></h2>
+            </div>
+        </div>
+    </div>
+  )
+}
