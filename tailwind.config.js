70fe8467474f2431ff68e48d5a8020b0a0d77ef8
AbimanyuBP <70093819+AbimanyuBP@users.noreply.github.com>
2024-06-19 14:10:13 +0700

    Added animations and update config requirements

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,32 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes:{
+        awooga: {
+          '0%': { width: '20%'},
+          '100%': { width: '100%'},
+        },
+        growabit: {
+          '0%, 100%': {transform: 'scale(1)'},
+          '50%': {transform: 'scale(1.01)'}
+        },
+        rightfade: {
+          '100%':{
+            transform: 'translate(30px,0)',
+            opacity: '0'
+          }
+        }
+      },
+      animation:{
+        'awooga': 'awooga 0.5s',
+        'growabit': 'growabit ease-in-out 3s infinite',
+        'rightfade': 'rightfade ease-in-out 2s infinite',
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    require("tailwindcss-animate"),
+  ],
 }
 
