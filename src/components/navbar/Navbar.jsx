7b56aa3e4a114472cdaa21cb52cfc6b449007a57
AbimanyuBP <70093819+AbimanyuBP@users.noreply.github.com>
2024-06-20 13:10:59 +0700

    Added better animation with transition to have better out

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,29 +8,34 @@ export default function Navbar() {
         <div className="flex-[6]">
             <ul className="flex py-[30px] pl-[20px] pr-[200px] justify-between">
                 <li className="group flex flex-col items-center">
-                    <hr className="bg-[black] h-[2px] group-hover:animate-awooga group-hover:fill-mode-forwards"/>
+                    <hr className="bg-white h-[2px] w-[0.1px] ease-in-out delay-150 group-hover:w-[100%] group-hover:bg-black duration-300"/>
                     <a href="/"><span className="text-[12px]">MAIN</span></a>
-                    <hr className="bg-[black] h-[2px] group-hover:animate-awooga group-hover:fill-mode-forwards"/>
+                    <hr className="bg-white h-[2px] w-[0.1px] ease-in-out delay-150 group-hover:w-[100%] group-hover:bg-black duration-300"/>
                 </li>
                 <li className="group flex flex-col items-center">
-                    <hr className="bg-[black] h-[2px] group-hover:animate-awooga group-hover:fill-mode-forwards"/>
+                    <hr className="bg-white h-[2px] w-[0.1px] ease-in-out delay-150 group-hover:w-[100%] group-hover:bg-black duration-300"/>
                     <a href="/gallery"><span className="text-[12px] cursor-pointer">GALLERY</span></a>
-                    <hr className="bg-[black] h-[2px] group-hover:animate-awooga group-hover:fill-mode-forwards"/>
+                    <hr className="bg-white h-[2px] w-[0.1px] ease-in-out delay-150 group-hover:w-[100%] group-hover:bg-black duration-300"/>
                 </li>
                 <li className="group flex flex-col items-center">
-                    <hr className="bg-[black] h-[2px] group-hover:animate-awooga group-hover:fill-mode-forwards"/>
+                    <hr className="bg-white h-[2px] w-[0.1px] ease-in-out delay-150 group-hover:w-[100%] group-hover:bg-black duration-300"/>
                     <a href="/projects"><span className="text-[12px] cursor-pointer">PROJECTS</span></a>
-                    <hr className="bg-[black] h-[2px] group-hover:animate-awooga group-hover:fill-mode-forwards"/>
+                    <hr className="bg-white h-[2px] w-[0.1px] ease-in-out delay-150 group-hover:w-[100%] group-hover:bg-black duration-300"/>
                 </li>
                 <li className="group flex flex-col items-center">
-                    <hr className="bg-[black] h-[2px] group-hover:animate-awooga group-hover:fill-mode-forwards"/>
+                    <hr className="bg-white h-[2px] w-[0.1px] ease-in-out delay-150 group-hover:w-[100%] group-hover:bg-black duration-300"/>
                     <a href="/certifications"><span className="text-[12px] cursor-pointer">CERTIFICATIONS</span></a>
-                    <hr className="bg-[black] h-[2px] group-hover:animate-awooga group-hover:fill-mode-forwards"/>
+                    <hr className="bg-white h-[2px] w-[0.1px] ease-in-out delay-150 group-hover:w-[100%] group-hover:bg-black duration-300"/>
                 </li>
                 <li className="group flex flex-col items-center">
-                    <hr className="bg-[black] h-[2px] group-hover:animate-awooga group-hover:fill-mode-forwards"/>
+                    <hr className="bg-white h-[2px] w-[0.1px] ease-in-out delay-150 group-hover:w-[100%] group-hover:bg-black duration-300"/>
                     <a href="/contacts"><span className="text-[12px] cursor-pointer">CONTACTS</span></a>
-                    <hr className="bg-[black] h-[2px] group-hover:animate-awooga group-hover:fill-mode-forwards"/>
+                    <hr className="bg-white h-[2px] w-[0.1px] ease-in-out delay-150 group-hover:w-[100%] group-hover:bg-black duration-300"/>
+                </li>
+                <li className="group flex flex-col items-center">
+                    <hr className="bg-white h-[2px] w-[0.1px] ease-in-out delay-150 group-hover:w-[100%] group-hover:bg-black duration-300"/>
+                    <a href="/contacts"><span className="text-[12px] cursor-pointer">CONTACTS</span></a>
+                    <hr className="bg-white h-[2px] w-[0.1px] ease-in-out delay-150 group-hover:w-[100%] group-hover:bg-black duration-300"/>
                 </li>
             </ul>
         </div>
