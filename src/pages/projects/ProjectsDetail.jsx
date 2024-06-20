722c57e35cced3982c832b9a5edf7b20ef7875c7
AbimanyuBP <70093819+AbimanyuBP@users.noreply.github.com>
2024-06-20 17:50:43 +0700

    Added new component

---
+++ src/pages/projects/ProjectsDetail.jsx
@@ -0,0 +1,12 @@
+import TitleText from "../../components/misc/TitleText";
+import Navbar from "../../components/navbar/Navbar";
+
+export default function ProjectsDetail() {
+  return (
+    <div>
+        <Navbar/>
+        <TitleText top="Sample" bottom="Project 1"/>
+        <Footer/>
+    </div>
+  )
+}
