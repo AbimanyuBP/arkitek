d8bd27b384b2a254fa80752b9a695373820505f0
AbimanyuBP <70093819+AbimanyuBP@users.noreply.github.com>
2024-06-20 17:50:15 +0700

    Added working image scroll logic

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -3,8 +3,27 @@ import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import HorizontalRuleIcon from '@mui/icons-material/HorizontalRule';
 import Stack from '@mui/material/Stack';
+import { useState } from 'react';
 
 export default function Hero() {
+    const projectList = [
+        {
+            img: "public/assets/projects/project_1.png"
+        },
+        {
+            img: "public/assets/projects/project_2.jpg"
+        },
+        {
+            img: "public/assets/projects/project_3.jpg"
+        },
+        {
+            img: "public/assets/projects/project_4.jpg"
+        },
+    ];
+
+    const [clicked, useClicked] = useState(0);
+
+
   return (
     <div className="flex justify-center items-center">
         <div className="flex-[2]">
@@ -13,16 +32,16 @@ export default function Hero() {
             </div>
             <div className="mx-[94px] mt-16">
                 <Stack spacing={2} direction="row">
-                    <Button variant="contained" sx={{backgroundColor: '#e2e2e2', "&:hover": {backgroundColor: "#818781"}}}><ArrowBackIcon className="text-[black]"/></Button>
-                    <Button variant="contained" sx={{backgroundColor: '#e2e2e2', "&:hover": {backgroundColor: "#818781"}}}><ArrowForwardIcon className="text-[black]"/></Button>
+                    <Button onClick={() => useClicked(clicked > 0 ? clicked - 1 : projectList.length-1)} variant="contained" sx={{backgroundColor: '#e2e2e2', "&:hover": {backgroundColor: "#818781"}}}><ArrowBackIcon className="text-[black]"/></Button>
+                    <Button onClick={() => useClicked(clicked == projectList.length-1 ? 0 : clicked+1)} variant="contained" sx={{backgroundColor: '#e2e2e2', "&:hover": {backgroundColor: "#818781"}}}><ArrowForwardIcon className="text-[black]"/></Button>
                 </Stack>
             </div>
             <div className="mx-[94px] mt-[90px]">
-                <h1 className="text-[30px]">1 <HorizontalRuleIcon sx={{fontSize: "50px", color: "gray"}} className="rotate-[-45deg]"/> 4</h1>
+                <h1 className="text-[30px]">{clicked+1} <HorizontalRuleIcon sx={{fontSize: "50px", color: "gray"}} className="rotate-[-45deg]"/> {projectList.length}</h1>
             </div>
         </div>
         <div className="flex-[4]">
-            <img src="assets\projects\project_1.png" className="w-[800px] h-[700px] object-cover" alt="" />
+            <img src={projectList[clicked].img} className="w-[800px] h-[700px] object-cover" alt="" />
             <div className="bg-white cursor-pointer w-[200px] h-[50px] relative bottom-[50px] flex items-center">
                 <h2 className="ml-[20px]">View Project <ArrowForwardIcon className="text-[black]" fontSize="small"/></h2>
             </div>
