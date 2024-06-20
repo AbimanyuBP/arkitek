a57702f61093461af539fac91ce3af7f24e25791
AbimanyuBP <70093819+AbimanyuBP@users.noreply.github.com>
2024-06-20 17:49:44 +0700

    Change link from a to Link

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
@@ -9,30 +10,30 @@ export default function Navbar() {
             <ul className="flex py-[30px] pl-[20px] pr-[200px] justify-between">
                 <li className="group flex flex-col items-center">
                     <hr className="bg-white h-[2px] w-[0.1px] ease-in-out delay-150 group-hover:w-[100%] group-hover:bg-black duration-300"/>
-                    <a href="/"><span className="text-[12px]">MAIN</span></a>
+                    <Link to={'/'}><span className="text-[12px]">MAIN</span></Link>
                     <hr className="bg-white h-[2px] w-[0.1px] ease-in-out delay-150 group-hover:w-[100%] group-hover:bg-black duration-300"/>
                 </li>
                 <li className="group flex flex-col items-center">
                     <hr className="bg-white h-[2px] w-[0.1px] ease-in-out delay-150 group-hover:w-[100%] group-hover:bg-black duration-300"/>
-                    <a href="/gallery"><span className="text-[12px] cursor-pointer">GALLERY</span></a>
+                    <Link to={'/gallery'}><span className="text-[12px] cursor-pointer">GALLERY</span></Link>
                     <hr className="bg-white h-[2px] w-[0.1px] ease-in-out delay-150 group-hover:w-[100%] group-hover:bg-black duration-300"/>
                 </li>
                 <li className="group flex flex-col items-center">
                     <hr className="bg-white h-[2px] w-[0.1px] ease-in-out delay-150 group-hover:w-[100%] group-hover:bg-black duration-300"/>
-                    <a href="/projects"><span className="text-[12px] cursor-pointer">PROJECTS</span></a>
+                    <Link to={'/projects'}><span className="text-[12px] cursor-pointer">PROJECTS</span></Link>
                     <hr className="bg-white h-[2px] w-[0.1px] ease-in-out delay-150 group-hover:w-[100%] group-hover:bg-black duration-300"/>
                 </li>
                 <li className="group flex flex-col items-center">
                     <hr className="bg-white h-[2px] w-[0.1px] ease-in-out delay-150 group-hover:w-[100%] group-hover:bg-black duration-300"/>
-                    <a href="/certifications"><span className="text-[12px] cursor-pointer">CERTIFICATIONS</span></a>
+                    <Link to={'/certifications'}><span className="text-[12px] cursor-pointer">CERTIFICATIONS</span></Link>
                     <hr className="bg-white h-[2px] w-[0.1px] ease-in-out delay-150 group-hover:w-[100%] group-hover:bg-black duration-300"/>
                 </li>
                 <li className="group flex flex-col items-center">
                     <hr className="bg-white h-[2px] w-[0.1px] ease-in-out delay-150 group-hover:w-[100%] group-hover:bg-black duration-300"/>
-                    <a href="/contacts"><span className="text-[12px] cursor-pointer">CONTACTS</span></a>
+                    <Link to={'/contacts'}><span className="text-[12px] cursor-pointer">CONTACTS</span></Link>
                     <hr className="bg-white h-[2px] w-[0.1px] ease-in-out delay-150 group-hover:w-[100%] group-hover:bg-black duration-300"/>
                 </li>
-                
+
             </ul>
         </div>
     </div>
