3c38b225770421a7a6dc12e7234d42fa35b112f1
Abimanyu B.P <abimanyubevanp@gmail.com>
2024-06-23 09:52:29 +0700

    Added placeholder data

---
+++ src/misc/projectList.js
@@ -0,0 +1,52 @@
+export const projectList = [
+    {
+        projectName: "Aulum",
+        imgUrl: "/assets/projects/project_1.png",
+        projectOverview: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ullamcorper sollicitudin ante et eleifend. Sed venenatis tellus metus, eget imperdiet dui consectetur tristique. Nullam rutrum lectus quam, ut luctus lacus ornare sit amet. Pellentesque congue suscipit turpis, dictum sollicitudin mauris pellentesque quis. Maecenas eget nunc ex. Nullam pretium viverra congue. Morbi ultricies dictum dolor, eu tristique elit blandit id. Aenean porta urna in posuere venenatis. Cras lobortis auctor eleifend. Nulla eget sapien non dolor cursus placerat nec sed ante. Nulla leo nibh, convallis eu pretium ac, ultrices vel sapien.",
+    },
+    {
+        projectName: "Navaro",
+        imgUrl: "/assets/projects/project_2.jpg",
+        projectOverview: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ullamcorper sollicitudin ante et eleifend. Sed venenatis tellus metus, eget imperdiet dui consectetur tristique. Nullam rutrum lectus quam, ut luctus lacus ornare sit amet. Pellentesque congue suscipit turpis, dictum sollicitudin mauris pellentesque quis. Maecenas eget nunc ex. Nullam pretium viverra congue. Morbi ultricies dictum dolor, eu tristique elit blandit id. Aenean porta urna in posuere venenatis. Cras lobortis auctor eleifend. Nulla eget sapien non dolor cursus placerat nec sed ante. Nulla leo nibh, convallis eu pretium ac, ultrices vel sapien.",
+    },
+    {
+        projectName: "Merry",
+        imgUrl: "/assets/projects/project_3.jpg",
+        projectOverview: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ullamcorper sollicitudin ante et eleifend. Sed venenatis tellus metus, eget imperdiet dui consectetur tristique. Nullam rutrum lectus quam, ut luctus lacus ornare sit amet. Pellentesque congue suscipit turpis, dictum sollicitudin mauris pellentesque quis. Maecenas eget nunc ex. Nullam pretium viverra congue. Morbi ultricies dictum dolor, eu tristique elit blandit id. Aenean porta urna in posuere venenatis. Cras lobortis auctor eleifend. Nulla eget sapien non dolor cursus placerat nec sed ante. Nulla leo nibh, convallis eu pretium ac, ultrices vel sapien.",
+    },
+    {
+        projectName: "Autumn",
+        imgUrl: "/assets/projects/project_4.jpg",
+        projectOverview: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ullamcorper sollicitudin ante et eleifend. Sed venenatis tellus metus, eget imperdiet dui consectetur tristique. Nullam rutrum lectus quam, ut luctus lacus ornare sit amet. Pellentesque congue suscipit turpis, dictum sollicitudin mauris pellentesque quis. Maecenas eget nunc ex. Nullam pretium viverra congue. Morbi ultricies dictum dolor, eu tristique elit blandit id. Aenean porta urna in posuere venenatis. Cras lobortis auctor eleifend. Nulla eget sapien non dolor cursus placerat nec sed ante. Nulla leo nibh, convallis eu pretium ac, ultrices vel sapien.",
+    },
+    {
+        projectName: "Breeze",
+        imgUrl: "/assets/projects/project_5.jpg",
+        projectOverview: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ullamcorper sollicitudin ante et eleifend. Sed venenatis tellus metus, eget imperdiet dui consectetur tristique. Nullam rutrum lectus quam, ut luctus lacus ornare sit amet. Pellentesque congue suscipit turpis, dictum sollicitudin mauris pellentesque quis. Maecenas eget nunc ex. Nullam pretium viverra congue. Morbi ultricies dictum dolor, eu tristique elit blandit id. Aenean porta urna in posuere venenatis. Cras lobortis auctor eleifend. Nulla eget sapien non dolor cursus placerat nec sed ante. Nulla leo nibh, convallis eu pretium ac, ultrices vel sapien.",
+    },
+    {
+        projectName: "Darum",
+        imgUrl: "/assets/projects/project_6.jpg",
+        projectOverview: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ullamcorper sollicitudin ante et eleifend. Sed venenatis tellus metus, eget imperdiet dui consectetur tristique. Nullam rutrum lectus quam, ut luctus lacus ornare sit amet. Pellentesque congue suscipit turpis, dictum sollicitudin mauris pellentesque quis. Maecenas eget nunc ex. Nullam pretium viverra congue. Morbi ultricies dictum dolor, eu tristique elit blandit id. Aenean porta urna in posuere venenatis. Cras lobortis auctor eleifend. Nulla eget sapien non dolor cursus placerat nec sed ante. Nulla leo nibh, convallis eu pretium ac, ultrices vel sapien.",
+    },
+    {
+        projectName: "Farud",
+        imgUrl: "/assets/projects/project_7.jpg",
+        projectOverview: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ullamcorper sollicitudin ante et eleifend. Sed venenatis tellus metus, eget imperdiet dui consectetur tristique. Nullam rutrum lectus quam, ut luctus lacus ornare sit amet. Pellentesque congue suscipit turpis, dictum sollicitudin mauris pellentesque quis. Maecenas eget nunc ex. Nullam pretium viverra congue. Morbi ultricies dictum dolor, eu tristique elit blandit id. Aenean porta urna in posuere venenatis. Cras lobortis auctor eleifend. Nulla eget sapien non dolor cursus placerat nec sed ante. Nulla leo nibh, convallis eu pretium ac, ultrices vel sapien.",
+    },
+    {
+        projectName: "Mellow",
+        imgUrl: "/assets/projects/project_8.jpg",
+        projectOverview: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ullamcorper sollicitudin ante et eleifend. Sed venenatis tellus metus, eget imperdiet dui consectetur tristique. Nullam rutrum lectus quam, ut luctus lacus ornare sit amet. Pellentesque congue suscipit turpis, dictum sollicitudin mauris pellentesque quis. Maecenas eget nunc ex. Nullam pretium viverra congue. Morbi ultricies dictum dolor, eu tristique elit blandit id. Aenean porta urna in posuere venenatis. Cras lobortis auctor eleifend. Nulla eget sapien non dolor cursus placerat nec sed ante. Nulla leo nibh, convallis eu pretium ac, ultrices vel sapien.",
+    },
+    {
+        projectName: "Celli",
+        imgUrl: "/assets/projects/project_9.jpg",
+        projectOverview: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ullamcorper sollicitudin ante et eleifend. Sed venenatis tellus metus, eget imperdiet dui consectetur tristique. Nullam rutrum lectus quam, ut luctus lacus ornare sit amet. Pellentesque congue suscipit turpis, dictum sollicitudin mauris pellentesque quis. Maecenas eget nunc ex. Nullam pretium viverra congue. Morbi ultricies dictum dolor, eu tristique elit blandit id. Aenean porta urna in posuere venenatis. Cras lobortis auctor eleifend. Nulla eget sapien non dolor cursus placerat nec sed ante. Nulla leo nibh, convallis eu pretium ac, ultrices vel sapien.",
+    },
+    {
+        projectName: "Verci",
+        imgUrl: "/assets/projects/project_10.jpg",
+        projectOverview: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ullamcorper sollicitudin ante et eleifend. Sed venenatis tellus metus, eget imperdiet dui consectetur tristique. Nullam rutrum lectus quam, ut luctus lacus ornare sit amet. Pellentesque congue suscipit turpis, dictum sollicitudin mauris pellentesque quis. Maecenas eget nunc ex. Nullam pretium viverra congue. Morbi ultricies dictum dolor, eu tristique elit blandit id. Aenean porta urna in posuere venenatis. Cras lobortis auctor eleifend. Nulla eget sapien non dolor cursus placerat nec sed ante. Nulla leo nibh, convallis eu pretium ac, ultrices vel sapien.",
+    },
+]
