92be129d8c3ae1b3a0b364de50229651998470e9
AbimanyuBP <70093819+AbimanyuBP@users.noreply.github.com>
2024-06-25 10:43:04 +0700

    Added rolling project overview list

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -8,6 +8,7 @@ import ArrowBackIcon from '@mui/icons-material/ArrowBack'
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward'
 import Stack from '@mui/material/Stack'
 import { projectList } from '../../misc/projectList';
+import { useState } from 'react';
 
 function ProjectCard({namaewa, desc, imgLink}){
     return(
@@ -23,21 +24,45 @@ function ProjectCard({namaewa, desc, imgLink}){
 }
 
 export default function Projects() {
+    const [clicked, useClicked] = useState(0);
+    const [start , useStart] = useState(0);
+    const [end, useEnd] = useState(5);
+    
+    function setter(clickedVal, startVal, endVal){
+        if(clickedVal >= projectList.length/5){
+            useClicked(0);
+            useStart(0);
+            useEnd(5);
+        }
+        else{
+            if(clickedVal < 0){
+                useClicked((projectList.length/5)-1);
+                useStart(start+5);
+                useEnd(end+5);
+            }
+            else{
+                useClicked(clickedVal);
+                useStart(startVal);
+                useEnd(endVal);
+            }
+        }
+    }
+
   return (
     <div>
         <Navbar/>
         <TitleText top="Our" bottom="Projects"/>
-        {projectList.slice(0-3).map((project) => (
+        {projectList.slice(start, end).map((project) => (
                 <ProjectCard namaewa={project.projectName} desc={project.projectOverview} imgLink={project.imgUrl}/>
                 ))}
         <div className="flex *:ml-20 *:my-10">
           <div className="">
-              <h1 className="text-[30px]">1 <HorizontalRuleIcon sx={{fontSize: "50px", color: "gray"}} className="rotate-[-45deg]"/> 4</h1>
+              <h1 className="text-[30px]">{clicked+1} <HorizontalRuleIcon sx={{fontSize: "50px", color: "gray"}} className="rotate-[-45deg]"/> {projectList.length/5}</h1>
           </div>
           <div className="">
                 <Stack spacing={2} direction="row">
-                    <Button variant="contained" sx={{backgroundColor: '#e2e2e2', "&:hover": {backgroundColor: "#818781"}}}><ArrowBackIcon className="text-[black]"/></Button>
-                    <Button variant="contained" sx={{backgroundColor: '#e2e2e2', "&:hover": {backgroundColor: "#818781"}}}><ArrowForwardIcon className="text-[black]"/></Button>
+                    <Button onClick={() => {setter(clicked-1, start-5, end-5)}} variant="contained" sx={{backgroundColor: '#e2e2e2', "&:hover": {backgroundColor: "#818781"}}}><ArrowBackIcon className="text-[black]"/></Button>
+                    <Button onClick={() => {setter(clicked+1, start+5, end+5)}} variant="contained" sx={{backgroundColor: '#e2e2e2', "&:hover": {backgroundColor: "#818781"}}}><ArrowForwardIcon className="text-[black]"/></Button>
                 </Stack>
           </div>
         </div>
